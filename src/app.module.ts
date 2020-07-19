cd59b7252e0374195d06f866add61dba3e3ddda6
mustapha chatbi <chatbimdev@gmail.com>
2020-07-19 17:20:33 +0100

    dynamic env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+let envPath = '.env.development';
+process.env.NODE_ENV = 'development';
+
+if (process.env.NODE_ENV == 'production') envPath = '.env.production';
+else if (process.env.NODE_ENV == 'staging') envPath = '.env.staging';
+
+const con = new ConfigService();
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuthModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: envPath,
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+console.log(con.get('title'), 'cooon');
