7788ae9de9892ef386ab658e60f2237a08b41a46
mustapha chatbi <chatbimdev@gmail.com>
2020-07-19 13:47:21 +0100

    finished auth ( register , login )

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [TypeOrmModule.forRoot(), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,66 @@
+import { debug } from 'console';
+import { Injectable, Logger } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/users.entity';
+import { UserRo } from '../users/users.ro';
+import { JwtPayload } from './interface/jwt-payload.interface';
+import { RegistrationStatus } from './interface/registrationStatus.interface';
+import { CreateUserDto } from './../users/users.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  private readonly logger = new Logger(AuthService.name);
+
+  async register(user: CreateUserDto) {
+    let status: RegistrationStatus = {
+      success: true,
+      message: 'user register',
+    };
+    try {
+      await this.usersService.register(user);
+    } catch (err) {
+      //debug(err);
+      status = { success: false, message: err };
+    }
+    return status;
+  }
+  createToken(user: User) {
+    //debug('get the expiration');
+    const expiresIn = 3600;
+    //debug('sign the token');
+    //debug(user);
+
+    const accessToken = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        firstname: user.firstName,
+        lastname: user.lastName,
+      },
+      'Codebrains',
+      { expiresIn },
+    );
+    //debug('return the token');
+    //debug(accessToken);
+    return {
+      expiresIn,
+      accessToken,
+    };
+  }
+
+  async validateUserToken(payload: JwtPayload): Promise<User> {
+    return await this.usersService.findById(payload.id);
+  }
+  async validateUser(email: string, password: string): Promise<UserRO> {
+    const user = await this.usersService.findByEmail(email);
+    if (user && user.comparePassword(password)) {
+      this.logger.log('password check success');
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
