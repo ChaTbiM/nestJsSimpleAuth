475ceb9fcf0bbc479ec9bb9ac1906d17272c362f
mustapha chatbi <chatbimdev@gmail.com>
2020-07-20 14:22:27 +0100

    generated documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('NestTest Api')
+    .setDescription('simple auth api , retrieve private or public books')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('documentation', app, document);
 
   await app.listen(3000);
 }
